2f1dfc182a2ebab20387681006ceb3ba65687a58
codebender828 <jbakebwa@163.com>
2019-10-12 18:43:12 +0800

    docs: how to provide theme with ThemePRovider

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-// import { provideTheme } from '@/lib/utils'
 import Kiwi from './lib/plugin'
 
 Vue.config.productionTip = false
 
+// Install Kiwi plugin
 Vue.use(Kiwi)
 
 new Vue({
+  // Alternative way to provide theme would be to import { provideTheme } from utils and provide it with render function
   // Provide theme to the root of the application
   // render: h => provideTheme(h, App)
   render: h => h(App)

7cbbf692703e0f2de060f1636e1a19b87549488c
codebender828 <jbakebwa@163.com>
2019-10-12 16:42:53 +0800

    add: theme provider component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ greeting }}
+    <k-button>Simple text</k-button>
+  </div>
+</template>
+
+<script>
+import KButton from './components/Button/index'
+
+export default {
+  name: 'App',
+  components: {
+    KButton
+  },
+  data () {
+    return {
+      greeting: 'Hi!'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import ThemeProvider from './components/ThemeContext/index.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(ThemeProvider, {}, [h(App)])
 }).$mount('#app')

8987a55dacdc54f0f2cc77556def1d80431d08a4
codebender828 <jbakebwa@163.com>
2019-11-09 02:05:21 +0800

    fix(badge): updated badge tests

--- src/components/Badge/badge.test.js
+++ src/components/Badge/badge.test.js
@@ -1,12 +1,23 @@
 import { shallowMount } from '@vue/test-utils'
-import Badge from './index.vue'
+import Badge from './'
+import ThemeProvider from '../ThemeProvider'
+import theme from '../../lib/theme'
 
 describe('===== Badge Component =====', () => {
-  let badge
   describe('Instance TEsts', () => {
     it('should be a Vue instance', () => {
-      badge = shallowMount(Badge)
-      expect(badge.isVueInstance()).toBeTruthy()
+      const wrapper = shallowMount(ThemeProvider, {
+        propsData: {
+          theme,
+          colorMode: 'light'
+        },
+        slots: {
+          default: [Badge]
+        }
+      })
+      expect(wrapper.isVueInstance()).toBeTruthy()
+      expect(wrapper.find(Badge).vm.$theme()).toBe(theme)
+      expect(wrapper.find(Badge).vm.$colorMode).toBe('light')
     })
   })
 })

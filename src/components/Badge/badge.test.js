12b7a759c7371318d6be8eea866dfe948410dead
codebender828 <jbakebwa@163.com>
2019-11-09 02:08:07 +0800

    refactor(badge): provide injects on shallowmount

--- src/components/Badge/badge.test.js
+++ src/components/Badge/badge.test.js
@@ -1,23 +1,21 @@
 import { shallowMount } from '@vue/test-utils'
 import Badge from './'
-import ThemeProvider from '../ThemeProvider'
 import theme from '../../lib/theme'
 
 describe('===== Badge Component =====', () => {
   describe('Instance TEsts', () => {
     it('should be a Vue instance', () => {
-      const wrapper = shallowMount(ThemeProvider, {
-        propsData: {
-          theme,
-          colorMode: 'light'
-        },
-        slots: {
-          default: [Badge]
+      const badge = shallowMount(Badge, {
+        provide () {
+          return {
+            $theme: () => theme,
+            $colorMode: 'light'
+          }
         }
       })
-      expect(wrapper.isVueInstance()).toBeTruthy()
-      expect(wrapper.find(Badge).vm.$theme()).toBe(theme)
-      expect(wrapper.find(Badge).vm.$colorMode).toBe('light')
+      expect(badge.isVueInstance()).toBeTruthy()
+      expect(badge.vm.$theme()).toBe(theme)
+      expect(badge.vm.$colorMode).toBe('light')
     })
   })
 })

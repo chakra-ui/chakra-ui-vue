6d3a33c41557a1063fc5b37e60f2c4bbe63e2cea
codebender828 <jbakebwa@163.com>
2019-11-08 20:27:56 +0800

    feat(badge): added badge storybook

---
+++ stories/8-Badge.stories.js
@@ -0,0 +1,25 @@
+import { storiesOf } from '@storybook/vue'
+import centered from '@storybook/addon-centered/vue'
+import Badge from '../src/components/Badge'
+
+storiesOf('UI | Badge', module)
+  .addDecorator(centered)
+  .add('Default Badge', () => ({
+    components: { Badge },
+    template: `
+      <div>
+        <Badge>Default</Badge>
+      </div>
+    `
+  }))
+  .add('With color', () => ({
+    components: { Badge },
+    template: `
+      <div>
+        <Badge mx="2">Default</Badge>
+        <Badge mx="2" variant-color="green">Success</Badge>
+        <Badge mx="2" variant-color="red">Removed</Badge>
+        <Badge mx="2" variant-color="indigo">New</Badge>
+      </div>
+    `
+  }))

ff5136750b4a17d1f0747e17abf765850c59f522
codebender828 <jbakebwa@163.com>
2019-12-03 10:42:23 +0800

    chore(lerna): publishing package

--- packages/kiwi-ui/src/Box/index.js
+++ packages/kiwi-ui/src/Box/index.js
@@ -63,7 +63,7 @@ const system = compose(
 
 /**
  * The Box component is the base reusable component which is the building block for all other Kiwi UI components.
- * It by default renders the `<div/>` element.
+ * It by default renders the `<div/>`  HTMLElement.
  */
 const Box = styled('div', {
   ...baseProps

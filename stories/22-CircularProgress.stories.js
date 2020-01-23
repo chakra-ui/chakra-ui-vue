595372b5eafa91301baea01293d01f891e1da304
codebender828 <jbakebwa@163.com>
2020-01-23 22:06:11 +0800

    feat(code): added code test cases and stories

---
+++ stories/22-CircularProgress.stories.js
@@ -0,0 +1,79 @@
+import { storiesOf } from '@storybook/vue'
+import centered from '@storybook/addon-centered/vue'
+import { CSSReset, CircularProgress, CircularProgressLabel, Button } from 'kiwi-core'
+
+storiesOf('UI | CircularProgress', module)
+  .addDecorator(centered)
+  .add('default circular progress', () => ({
+    components: { CSSReset, CircularProgress, CircularProgressLabel, Button },
+    template: `,
+      <div>
+        <CSSReset />
+        <CircularProgress :value="60" />
+      </div>
+    `
+  }))
+  .add('with label', () => ({
+    components: { CSSReset, CircularProgress, CircularProgressLabel, Button },
+    template: `,
+      <div>
+        <CSSReset />
+        <CircularProgress :value="value">
+          <CircularProgressLabel>{{ value }}%</CircularProgressLabel>
+        </CircularProgress>
+      </div>
+    `,
+    data () {
+      return {
+        value: 60
+      }
+    }
+  }))
+  .add('With size', () => ({
+    components: { CSSReset, CircularProgress, CircularProgressLabel, Button },
+    template: `,
+      <div>
+        <CSSReset />
+        <CircularProgress size="120px" :value="value">
+          <CircularProgressLabel>{{ value }}%</CircularProgressLabel>
+        </CircularProgress>
+      </div>
+    `,
+    data () {
+      return {
+        value: 60
+      }
+    }
+  }))
+  .add('With color', () => ({
+    components: { CSSReset, CircularProgress, CircularProgressLabel, Button },
+    template: `,
+      <div>
+        <CSSReset />
+        <CircularProgress size="120px" color="blue" :value="value">
+          <CircularProgressLabel>{{ value }}%</CircularProgressLabel>
+        </CircularProgress>
+      </div>
+    `,
+    data () {
+      return {
+        value: 60
+      }
+    }
+  }))
+  .add('With thickness', () => ({
+    components: { CSSReset, CircularProgress, CircularProgressLabel, Button },
+    template: `,
+      <div>
+        <CSSReset />
+        <CircularProgress size="120px" :thickness="0.1" :value="value">
+          <CircularProgressLabel>{{ value }}%</CircularProgressLabel>
+        </CircularProgress>
+      </div>
+    `,
+    data () {
+      return {
+        value: 60
+      }
+    }
+  }))

7eda1452d4ff1c7c6c2b5c37ae69c14c54bf4bfe
codebender828 <jbakebwa@163.com>
2020-02-20 01:33:02 +0800

    chore(formcontrol): add form helper text stories

--- stories/33-FormControl.stories.js
+++ stories/33-FormControl.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/vue'
-import { Input, FormControl, FormLabel } from '../packages/kiwi-core/src'
+import { Input, FormControl, FormLabel, Icon, Stack, InputGroup, InputLeftElement, InputRightElement, FormHelperText, FormErrorMessage } from '../packages/kiwi-core/src'
 
 storiesOf('UI | FormControl', module)
   .add('Basic Usage', () => ({
@@ -10,4 +10,44 @@ storiesOf('UI | FormControl', module)
         <Input id="fname" placeholder="First name" />
       </FormControl>
     `
+  }))
+  .add('With Form Helper Text', () => ({
+    components: { Input, FormControl, Stack, Icon, InputGroup, InputLeftElement, InputRightElement, FormHelperText },
+    template: `
+      <div>
+        <FormControl isInvalid id="test-error">
+          <InputGroup>
+            <InputLeftElement color="gray.300" fontSize="1.2em"><Icon name="lock" /></InputLeftElement>
+            <Input :type="shouldShowPassword ? 'text' : 'password'" placeholder="Password" />
+            <InputRightElement @click.native="shouldShowPassword = !shouldShowPassword" ><Icon :name="shouldShowPassword ? 'eye-slash' : 'eye'" color="gray.500" /></InputRightElement>
+          </InputGroup>
+          <FormHelperText>Add your website here</FormHelperText>
+        </FormControl>
+      </div>
+    `,
+    data () {
+      return {
+        shouldShowPassword: false
+      }
+    }
+  }))
+  .add('With Form Validation Message', () => ({
+    components: { Input, FormControl, Stack, Icon, InputGroup, InputLeftElement, InputRightElement, FormErrorMessage },
+    template: `
+      <div>
+        <FormControl isInvalid id="test-error">
+          <InputGroup>
+            <InputLeftElement color="gray.300" fontSize="1.2em"><Icon name="lock" /></InputLeftElement>
+            <Input :type="shouldShowPassword ? 'text' : 'password'" placeholder="Password" />
+            <InputRightElement @click.native="shouldShowPassword = !shouldShowPassword" ><Icon :name="shouldShowPassword ? 'eye-slash' : 'eye'" color="gray.500" /></InputRightElement>
+          </InputGroup>
+          <FormErrorMessage id="url-error">Website is invalid</FormErrorMessage>
+        </FormControl>
+      </div>
+    `,
+    data () {
+      return {
+        shouldShowPassword: false
+      }
+    }
   }))

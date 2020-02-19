0e38a74a654f1cfadf2cb76f05aabec687f4b057
codebender828 <jbakebwa@163.com>
2020-02-20 02:25:38 +0800

    feat: add dev rollup config

--- packages/kiwi-core/rollup.dev.config.js
+++ packages/kiwi-core/rollup.dev.config.js
@@ -1,9 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import cjs from 'rollup-plugin-commonjs'
-import {
-  terser
-} from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser'
 import buble from 'rollup-plugin-buble'
 import vue from 'rollup-plugin-vue'
 import pkg from './package.json'
@@ -68,14 +66,7 @@ const commons = {
   ]
 }
 
-const bannerTxt = `/*! Chakra-ui/vue v${pkg.version} | MIT License | github.com/codebender828/kiwi-ui */`
-
 const baseFolder = './src/'
-
-const capitalize = (s) => {
-  if (typeof s !== 'string') return ''
-  return s.charAt(0).toUpperCase() + s.slice(1)
-}
 
 const components = fs.readdirSync(baseFolder)
   .filter((f) => fs.statSync(path.join(baseFolder, f)).isDirectory())
@@ -99,41 +90,7 @@ export default () => {
       format: 'esm'
     },
     ...commons
-  },
-  {
-    input: entries,
-    output: {
-      dir: `dist/cjs/`,
-      format: 'cjs',
-      exports: 'named'
-    },
-    ...commons
-  },
-  {
-    input: './src/index.js',
-    output: {
-      file: `dist/umd/index.js/`,
-      name: capitalize('chakra'),
-      format: 'umd',
-      exports: 'named',
-      banner: bannerTxt,
-      globals: {
-        vue: 'Vue'
-      }
-    },
-    ...commons
   }
   ]
-  if (process.env.MINIFY === 'true') {
-    config = config.filter((c) => !!c.output.file)
-    config.forEach((c) => {
-      c.output.file = c.output.file.replace(/\.js/g, '.min.js')
-      c.plugins.push(terser({
-        output: {
-          comments: '/^!/'
-        }
-      }))
-    })
-  }
   return config
 }

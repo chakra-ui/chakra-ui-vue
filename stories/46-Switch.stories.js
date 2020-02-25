55049b1807b6917f8b96f7e31e3f190d9c5b1915
codebender828 <jbakebwa@163.com>
2020-02-26 02:29:09 +0800

    feat(switch): added switch stories

---
+++ stories/46-Switch.stories.js
@@ -0,0 +1,28 @@
+import { storiesOf } from '@storybook/vue'
+import { Box, Switch } from '../packages/kiwi-core/src'
+
+storiesOf('UI | Switch', module)
+  .add('Basic Usage', () => ({
+    components: { Box, Toggle: Switch },
+    template: `
+      <Box mb="3">
+        <Toggle
+          size="sm"
+          @change="handleChange"
+          color="green"
+          mr="3"
+        />
+        <Toggle
+          size="md"
+          @change="handleChange"
+          color="blue"
+          mr="3"
+        />
+        <Toggle
+          size="lg"
+          @change="handleChange"
+          color="cyan"
+        />
+      </Box>
+    `
+  }))

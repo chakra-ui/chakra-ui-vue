e1c6900f748d78d6a9e42f74b58ac6c0757c8a89
codebender828 <jbakebwa@163.com>
2020-02-26 02:26:56 +0800

    feat(switch): added switch component

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,39 @@
 <template>
   <div>
-      <Box mb="3">
-        <Tag v-for="size in ['sm', 'md', 'lg']" :size="size" :key="size" mx="1" variantColor="green">
-          <TagIcon icon="add" size="12px" />
-          <TagLabel>Green</TagLabel>
-        </Tag>
-      </Box>
-      <!-- With right Icon -->
-
-      <Box mb="3">
-        <Tag v-for="size in ['sm', 'md', 'lg']" :size="size" :key="size + 'indigo'" mx="1" variantColor="indigo">
-          <TagLabel>Green</TagLabel>
-          <TagIcon icon="sun" size="12px" />
-        </Tag>
-      </Box>
-
-      <!-- With close button -->
-      <Box mb="3">
-        <Tag rounded="full" v-for="size in ['sm', 'md', 'lg']" :size="size" :key="size + 'cyan'" mx="1" variantColor="cyan">
-          <TagLabel>Green</TagLabel>
-          <TagCloseButton icon="sun" size="12px" />
-        </Tag>
-      </Box>
-
-      <!-- With custom element -->
-      <Box mb="3">
-        <Tag rounded="full" mx="1" variantColor="red">
-          <Avatar
-            name="Jonathan Bakebwa"
-            size="xs"
-            src="https://res.cloudinary.com/xtellar/image/upload/v1572857445/me_zqos4e.jpg"
-            :ml="-2"
-            :mr="2"
-           />
-          <TagLabel>Jonathan</TagLabel>
-        </Tag>
-      </Box>
+    <!-- With custom element -->
+    <Box mb="3">
+      <Toggle
+        size="sm"
+        @change="handleChange"
+        color="green"
+        mr="3"
+      />
+      <Toggle
+        size="md"
+        @change="handleChange"
+        color="blue"
+        mr="3"
+      />
+      <Toggle
+        size="lg"
+        @change="handleChange"
+        color="cyan"
+      />
+    </Box>
   </div>
 </template>
 
 <script lang="js">
 import {
   Box,
-  Tag,
-  TagLabel,
-  TagIcon,
-  TagCloseButton,
-  Avatar } from '../packages/kiwi-core/dist/esm'
+  Switch } from '../packages/kiwi-core/dist/esm'
 
 export default {
   name: 'App',
   inject: ['$theme', '$colorMode'],
   components: {
-    Tag,
-    TagLabel,
-    TagIcon,
     Box,
-    TagCloseButton,
-    Avatar
+    Toggle: Switch
   },
   data () {
     return {

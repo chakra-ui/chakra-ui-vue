1fbd97279c78c4d06df5ada62333287a7fcdc5c5
Mesut Koca <imesutkoca@gmail.com>
2020-03-01 02:07:00 +0300

    fix(tests): add eslint support for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,8 @@ module.exports = {
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)'
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/*.{j,t}s?(x)'
       ],
       env: {
         jest: true

86038ca2e6a9626ff92dee388c83658ff93d4374
Mesut Koca <imesutkoca@gmail.com>
2020-03-01 20:48:20 +0300

    test(breadcrumb):  add tests for breadcrumb component

---
+++ packages/kiwi-core/src/Breadcrumb/tests/Breadcrumb.test.js
@@ -0,0 +1,52 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from '../'
+import { render } from '@/packages/kiwi-core/src/test-utils'
+import CompositionApi from '@vue/composition-api'
+
+const renderComponent = (props) => {
+  const base = {
+    components: {
+      Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator
+    },
+    template: `
+    <div>
+      <Breadcrumb :add-separator="false">
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#">Breadcrumb 1</BreadcrumbLink>
+          <BreadcrumbSeparator color="tomato" font-size="10px" font-weight="bold" />
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#">Breadcrumb 2</BreadcrumbLink>
+          <BreadcrumbSeparator color="firebrick" font-size="20px" font-weight="bold" />
+        </BreadcrumbItem>
+      </Breadcrumb>
+    </div>
+   `,
+    ...props
+  }
+  return render(base, {}, vue => {
+    vue.use(CompositionApi)
+  })
+}
+
+it('should render correctly', () => {
+  const { asFragment } = renderComponent()
+  expect(asFragment()).toMatchSnapshot()
+})
+
+it('should display custom seperator ', () => {
+  const { asFragment } = renderComponent({
+    template: `
+    <Breadcrumb separator="-">
+      <BreadcrumbItem>
+        <BreadcrumbLink href="/">Home</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbItem>
+        <BreadcrumbLink href="/about">About</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbItem isCurrentPage>
+        <BreadcrumbLink href="/contact">Contact</BreadcrumbLink>
+      </BreadcrumbItem>
+    </Breadcrumb>`
+  })
+  expect(asFragment()).toMatchSnapshot()
+})

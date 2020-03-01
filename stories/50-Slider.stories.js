bc8dc2b5415784802f0718dcbd0bbe3214765e49
codebender828 <jbakebwa@163.com>
2020-03-01 17:26:39 +0800

    feat(slider): added slider stories

--- stories/50-Slider.stories.js
+++ stories/50-Slider.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/vue'
-import { Box, Slider, SliderTrack, SliderFilledTrack, SliderThumb } from '../packages/kiwi-core/src'
+import { Box, Slider, SliderTrack, SliderFilledTrack, SliderThumb, Icon } from '../packages/kiwi-core/src'
 
 storiesOf('UI | Slider', module)
   .add('Basic Usage', () => ({
@@ -10,6 +10,20 @@ storiesOf('UI | Slider', module)
           <SliderTrack />
           <SliderFilledTrack />
           <SliderThumb />
+        </Slider>
+      </Box>
+    `
+  }))
+  .add('Customized Slider', () => ({
+    components: { Box, Icon, Slider, SliderTrack, SliderFilledTrack, SliderThumb },
+    template: `
+      <Box w="300px">
+        <Slider :defaultValue="30">
+          <SliderTrack bg="red.100" />
+          <SliderFilledTrack bg="tomato" />
+          <SliderThumb p="3" d="flex" alignItems="center" justifyContent="center">
+            <Icon name="coffee" color="tomato" />
+          </SliderThumb>
         </Slider>
       </Box>
     `

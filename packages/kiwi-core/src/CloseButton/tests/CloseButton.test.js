de2de05f1352da29a6d381e6be8dedda4052b741
Mesut Koca <imesutkoca@gmail.com>
2020-03-01 14:58:56 +0300

    test(closebutton): add tests for closebutton component

---
+++ packages/kiwi-core/src/CloseButton/tests/CloseButton.test.js
@@ -0,0 +1,25 @@
+import CloseButton from '../'
+import { render } from '@/packages/kiwi-core/src/test-utils'
+
+const renderComponent = (props) => {
+  const base = {
+    components: { CloseButton },
+    template: `<CloseButton />`,
+    ...props
+  }
+  return render(base)
+}
+it('should render correctly', () => {
+  const { asFragment } = renderComponent({
+    template: `<CloseButton />`
+  })
+  expect(asFragment()).toMatchSnapshot()
+})
+it('should allow setting a custom aria-label for the button', () => {
+  const { container } = renderComponent({ template: `<CloseButton aria-label="my aria label" />` })
+
+  expect(container.querySelector('button')).toHaveAttribute(
+    'aria-label',
+    'my aria label'
+  )
+})

018a725cd85bb1ffeaf7c50ec26cbbcac5013536
codebender828 <jbakebwa@163.com>
2020-03-02 15:05:40 +0800

    chore(transition): update transition component

--- packages/kiwi-core/src/Transition/index.js
+++ packages/kiwi-core/src/Transition/index.js
@@ -1,31 +1,6 @@
-import { computed, createElement as h } from '@vue/composition-api'
 import anime from 'animejs'
 import { isUndef, isVueComponent, cloneVNodeElement, cleanChildren } from '../utils'
 import Box from '../Box'
-
-/**
- * Renders transition component's children
- * @param {Vue.PropOptions} props Current VNode props
- * @param {Vue.RenderContext} context Current VNode's render context
- * @param {{enter: Function, leave: Function}} handlers Transition event handlers
- * @returns {Vue.VNode} Children
- */
-function renderChildren (props, context, { enter, leave }) {
-  const children = context.slots.default()
-  const TransitionElement = children.length > 1 ? 'TransitionGroup' : 'Transition'
-  return h(TransitionElement, {
-    props: {
-      css: false
-    },
-    on: {
-      beforeEnter (el) {
-        el && el.style.setProperty('will-change', 'opacity, transform')
-      },
-      enter,
-      leave
-    }
-  }, props.in && context.slots.default())
-}
 
 const enterEasing = 'spring(1, 100, 50, 0)'
 const leaveEasing = 'spring(1, 100, 70, 0)'
@@ -33,87 +8,110 @@ const leaveEasing = 'spring(1, 100, 70, 0)'
 const Slide = {
   name: 'Slide',
   props: {
-    in: {
-      type: Boolean,
-      default: false
-    },
-    duration: {
-      type: Number,
-      default: 250
-    },
-    finalHeight: {
-      type: String,
-      default: 'auto'
-    },
-    from: {
-      type: String,
-      validator: (value) => value.match(/^(top|right|bottom|left)$/)
-    },
-    finalWidth: String,
-    delay: {
+    initialHeight: {
       type: Number,
       default: 0
     },
-    easing: String
+    duration: {
+      type: Number,
+      default: 150
+    },
+    enterEasing: {
+      type: String,
+      default: enterEasing
+    },
+    leaveEasing: {
+      type: String,
+      default: leaveEasing
+    },
+    finalHeight: Number,
+    animateOpacity: {
+      type: Boolean,
+      default: true
+    },
+    from: {
+      type: String,
+      default: 'bottom'
+    }
   },
-  setup (props, context) {
-    if (isUndef(props.from)) {
+  data () {
+    return {
+      transitionOptions: {
+        bottom: {
+          offset: '-100%',
+          transform: 'translateY'
+        },
+        top: {
+          offset: '100%',
+          transform: 'translateY'
+        },
+        left: {
+          offset: '100%',
+          transform: 'translateX'
+        },
+        right: {
+          offset: '-100%',
+          transform: 'translateX'
+        }
+      }
+    }
+  },
+  computed: {
+    transform () {
+      return this.transitionOptions[this.from].transform
+    },
+    transitions () {
+      return {
+        enter: {
+          [this.transform]: ['0%', this.transitionOptions[this.from]['offset']],
+          opacity: [0, 1]
+        },
+        leave: {
+          [this.transform]: [this.transitionOptions[this.from]['offset'], '0%'],
+          opacity: 0
+        }
+      }
+    }
+  },
+  methods: {
+    enter (el, complete) {
+      anime({
+        targets: el,
+        ...this.transitions['enter'],
+        complete,
+        easing: this.enterEasing
+      })
+    },
+
+    leave (el, complete) {
+      anime({
+        targets: el,
+        ...this.transitions['leave'],
+        complete,
+        easing: this.leaveEasing
+      })
+    }
+  },
+  render (h) {
+    if (isUndef(this.from)) {
       console.error('[Chakra]: The Slide component expected prop "from" but none was passed.')
       return () => null
     }
 
-    let transitionOptions = {
-      bottom: {
-        offset: '-100%',
-        transform: 'translateY'
+    const children = this.$slots.default
+    const TransitionElement = children.length > 1 ? 'TransitionGroup' : 'Transition'
+    return h(TransitionElement, {
+      props: {
+        css: false
       },
-      top: {
-        offset: '100%',
-        transform: 'translateY'
-      },
-      left: {
-        offset: '100%',
-        transform: 'translateX'
-      },
-      right: {
-        offset: '-100%',
-        transform: 'translateX'
-      }
-    }
-
-    const transform = computed(() => transitionOptions[props.from].transform)
-    const transitions = computed(() => {
-      return {
-        enter: {
-          [transform.value]: ['0%', transitionOptions[props.from]['offset']],
-          opacity: [0, 1]
+      on: {
+        beforeEnter (el) {
+          el && el.style.setProperty('will-change', 'opacity, transform')
         },
-        leave: {
-          [transform.value]: [transitionOptions[props.from]['offset'], '0%'],
-          opacity: 0
-        }
+        enter: this.enter,
+        leave: this.enter
       }
-    })
-
-    const enter = (el, complete) => {
-      anime({
-        targets: el,
-        ...transitions.value['enter'],
-        complete,
-        easing: props.easing || enterEasing
-      })
-    }
-
-    const leave = (el, complete) => {
-      anime({
-        targets: el,
-        ...transitions.value['leave'],
-        complete,
-        easing: props.easing || leaveEasing
-      })
-    }
-
-    return () => renderChildren(props, context, { enter, leave })
+    }, this.$slots.default)
   }
 }
 
@@ -276,7 +274,6 @@ const Fade = {
 const SlideIn = {
   name: 'SlideIn',
   props: {
-    in: Boolean,
     offset: {
       type: String,
       default: '10px'
@@ -286,28 +283,55 @@ const SlideIn = {
       default: 150
     }
   },
-  setup (props, context) {
-    const enter = (el, complete) => {
+  methods: {
+    enter (el, complete) {
       anime({
         targets: el,
         opacity: [0, 1],
-        translateY: [props.offset, '0px'],
+        translateY: [this.offset, '0px'],
         easing: enterEasing,
         complete
       })
-    }
-
-    const leave = (el, complete) => {
+    },
+    leave (el, complete) {
       anime({
         targets: el,
         opacity: [1, 0],
-        translateY: ['0px', props.offset],
+        translateY: ['0px', this.offset],
         easing: leaveEasing,
         complete
       })
     }
+  },
+  render (h) {
+    let finalChildren
+    const children = this.$slots.default || [h(null)]
 
-    return () => renderChildren(props, context, { enter, leave })
+    if (children.length > 1) {
+      const clean = cleanChildren(children)
+      finalChildren = clean.map((vnode, index) => {
+        return cloneVNodeElement(vnode, {
+          key: `scale-${index}`
+        }, h)
+      })
+    } else {
+      finalChildren = children
+    }
+
+    const TransitionElement = finalChildren.length > 1 ? 'TransitionGroup' : 'Transition'
+
+    return h(TransitionElement, {
+      props: {
+        css: false
+      },
+      on: {
+        beforeEnter (el) {
+          el && el.style.setProperty('will-change', 'opacity, transform')
+        },
+        enter: this.enter,
+        leave: this.leave
+      }
+    }, finalChildren)
   }
 }
 
--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
   },
   data () {
     return {
-      isOpen: false
+      isOpen: true
     }
   },
   computed: {

b5e59d8eb1c0de691621a02bb516ad96424c0aad
Mesut Koca <imesutkoca@gmail.com>
2020-03-09 22:24:58 +0300

    feat(storybook): sort stories a to z

--- .storybook/config.js
+++ .storybook/config.js
@@ -55,7 +55,9 @@ Vue.use(Kiwi, {
 
 addParameters({
   options: {
-   theme: storyBookTheme
+   theme: storyBookTheme,
+   storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
  }
 })
 

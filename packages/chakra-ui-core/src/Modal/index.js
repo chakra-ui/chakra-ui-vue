d226e501c9624af076732de3479df9df2268c9fa
Mesut Koca <imesutkoca@gmail.com>
2020-03-18 23:19:57 +0300

    fix(modal): disable inherit attrs for ModalContent component

--- packages/chakra-ui-core/src/Modal/index.js
+++ packages/chakra-ui-core/src/Modal/index.js
@@ -253,6 +253,7 @@ const ModalOverlay = {
 
 const ModalContent = {
   name: 'ModalContent',
+  inheritAttrs: false,
   inject: ['$ModalContext', '$colorMode'],
   props: {
     ...baseProps,
@@ -409,7 +410,8 @@ const ModalContent = {
           tabIndex: -1,
           id: contentId,
           ...(addAriaDescribedby && { 'aria-describedby': bodyId }),
-          ...(addAriaLabelledby && { 'aria-labelledby': headerId })
+          ...(addAriaLabelledby && { 'aria-labelledby': headerId }),
+          ...this.$attrs
         },
         nativeOn: {
           click: wrapEvent((e) => this.$emit('click', e), event => event.stopPropagation())

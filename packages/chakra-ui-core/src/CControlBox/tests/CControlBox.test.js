7c3511c905de0c05ecbfd330a96f0eda405c742e
Mesut Koca <imesutkoca@gmail.com>
2020-04-17 02:00:45 +0300

    tests(control-box): refactor and add controlled tests

--- packages/chakra-ui-core/src/CControlBox/tests/CControlBox.test.js
+++ packages/chakra-ui-core/src/CControlBox/tests/CControlBox.test.js
@@ -6,12 +6,13 @@ jest.mock('@/packages/chakra-ui-core/src/CToast/index.js', () => {})
 
 const renderComponent = (props) => {
   const inlineAttrs = (props && props.inlineAttrs) || ''
+  const type = props.type && `type="${props.type}"`
   const base = {
     components: { CVisuallyHidden, CControlBox, CBox },
     template: `
   <CBox as="label" display="flex" border-color="gray.200" align-items="center" cursor="pointer" for="control-radio">
     <!-- This is the sibling input, it's visually hidden -->
-    <CVisuallyHidden as="input" id="control-radio" data-testid="hiddenControl" ${inlineAttrs} />
+    <CVisuallyHidden as="input" id="control-radio" data-testid="hiddenControl" ${inlineAttrs} ${type} />
   
     <!-- This is the control box with a check icon as children -->
     <CControlBox
@@ -28,7 +29,6 @@ const renderComponent = (props) => {
       :_hover="{ borderColor: 'gray.300' }"
       :_focus="{ boxShadow: 'outline' }"
       :_disabled="{ opacity: '40%' }"
-      ${inlineAttrs}
     >
       <CBox w="50%" h="50%" bg="white" rounded="full" />
     </CControlBox>
@@ -41,43 +41,62 @@ const renderComponent = (props) => {
 }
 
 it('should render correctly', () => {
-  const inlineAttrs = `type="radio"`
-  const { asFragment } = renderComponent({ inlineAttrs })
+  const { asFragment } = renderComponent({ type: 'radio' })
   expect(asFragment()).toMatchSnapshot()
 })
 
 test('Uncontrolled radio - should be checked always', async () => {
-  const inlineAttrs = `type="radio"`
-  const { container, getByTestId } = renderComponent({ inlineAttrs })
-  const input = container.querySelector('input')
+  const { getByTestId } = renderComponent({ type: 'radio' })
   const control = getByTestId('control')
   const hiddenControl = getByTestId('hiddenControl')
 
   // click the first time, it's checked
   await userEvent.click(control)
-  expect(input).toBeChecked()
   expect(hiddenControl).toBeChecked()
 
   // click the second time, it should be still checked
   await userEvent.click(control)
-  expect(input).toBeChecked()
   expect(hiddenControl).toBeChecked()
 })
 
-test('Uncontrolled checkbox - should be checked always', async () => {
-  const inlineAttrs = `type="checkbox"`
-  const { container, getByTestId } = renderComponent({ inlineAttrs })
-  const input = container.querySelector('input')
+test('Uncontrolled checkbox - should toggle', async () => {
+  const { getByTestId } = renderComponent({ type: 'checkbox' })
   const control = getByTestId('control')
   const hiddenControl = getByTestId('hiddenControl')
 
   // click the first time, it's checked
   await userEvent.click(control)
-  expect(input).toBeChecked()
   expect(hiddenControl).toBeChecked()
 
   // click the second time, it's unchecked
   await userEvent.click(control)
-  expect(input).not.toBeChecked()
   expect(hiddenControl).not.toBeChecked()
+})
+
+test('controlled checkbox - v-model works', async () => {
+  const inlineAttrs = `:checked="checked"`
+  const { getByTestId } = renderComponent({ inlineAttrs, type: 'checkbox', data: () => ({ checked: true }) })
+  const control = getByTestId('control')
+  const hiddenControl = getByTestId('hiddenControl')
+
+  // click the first time, it's checked
+  expect(hiddenControl).toBeChecked()
+
+  // click the second time, it's unchecked
+  await userEvent.click(control)
+  expect(hiddenControl).not.toBeChecked()
+})
+
+test('controlled radio - v-model works', async () => {
+  const inlineAttrs = `:checked="checked"`
+  const { getByTestId } = renderComponent({ inlineAttrs, type: 'radio', data: () => ({ checked: true }) })
+  const control = getByTestId('control')
+  const hiddenControl = getByTestId('hiddenControl')
+
+  // click the first time, it's checked
+  expect(hiddenControl).toBeChecked()
+
+  // click the second time, it should be still checked
+  await userEvent.click(control)
+  expect(hiddenControl).toBeChecked()
 })

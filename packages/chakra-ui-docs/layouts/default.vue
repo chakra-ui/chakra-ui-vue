e221299705863087a461864c23ddabc39d4ab248
codebender828 <jbakebwa@163.com>
2020-05-09 03:42:20 +0800

    fix: light mode component

--- packages/chakra-ui-docs/layouts/default.vue
+++ packages/chakra-ui-docs/layouts/default.vue
@@ -136,42 +136,6 @@ export default {
     hash () {
       return this.$route.name
     }
-  },
-  // TODO: Setup title hashing
-  // mounted () {
-  //   this.setPageIds()
-  // },
-  watch: {
-    hash (newVal, oldVal) {
-      console.log('Hash changed', newVal)
-      // if (newVal !== oldVal) {
-      //   this.pushWindowToId()
-      // }
-    }
-  },
-  methods: {
-    /**
-     * TODO: Finish implementing this
-     * Sets the IDs for titles to allow for scrolling.
-     */
-    // setPageIds () {
-    //   const pageWrapper = document.getElementById('page-content')
-    //   const h2s = pageWrapper.querySelectorAll('h2')
-    //   const h3s = pageWrapper.querySelectorAll('h3')
-
-    //   const allTitles = [ ...h2s, ...h3s ]
-    //   allTitles.forEach(title => {
-    //     const content = `<div>${title.textContent}` + `<a aria-label="${title.textContent}" href="${stringToUrl(title.textContent, '#')}"></a></div>`
-    //     title.innerHTML = content
-    //   })
-
-    //   setTimeout(() => {
-    //     this.pushWindowToId()
-    //   })
-    // },
-    pushWindowToId () {
-      this.$router.push(this.$route.fullPath)
-    }
   }
 }
 </script>

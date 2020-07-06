e988316e121ceae0c14052b728b298c4be4ec7b2
Mesut Koca <imesutkoca@gmail.com>
2020-07-07 01:24:22 +0300

    test(input): add tests for input component and fix readonly

--- packages/chakra-ui-core/src/CInput/CInput.js
+++ packages/chakra-ui-core/src/CInput/CInput.js
@@ -90,7 +90,7 @@ const CInput = {
       },
       attrs: {
         'aria-readonly': this.isReadOnly,
-        'read-only': this.formControl.isReadOnly,
+        readonly: this.formControl.isReadOnly,
         disabled: this.formControl.isDisabled,
         'aria-disabled': this.formControl.isDisabled,
         'aria-invalid': this.formControl.isInvalid,

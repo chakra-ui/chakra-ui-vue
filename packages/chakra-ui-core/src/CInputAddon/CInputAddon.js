02bd7eb06e67a3d200b053390a0d84226d3446d7
codebender828 <jbakebwa@163.com>
2020-07-11 15:15:34 +0800

    refactor(input): update input addon compoennts to use  api

--- packages/chakra-ui-core/src/CInputAddon/CInputAddon.js
+++ packages/chakra-ui-core/src/CInputAddon/CInputAddon.js
@@ -9,9 +9,7 @@
 
 import styleProps from '../config/props'
 import useInputStyle from '../CInput/utils/input.styles'
-import { forwardProps } from '../utils'
-
-import CBox from '../CBox'
+import { forwardProps, createStyledAttrsMixin } from '../utils'
 
 const addonProps = {
   ...styleProps,
@@ -34,53 +32,43 @@ const addonProps = {
  * @see Docs https://vue.chakra-ui.com/input
  */
 const CInputAddon = {
-  name: 'CInputAddon',
-  inject: ['$chakraColorMode', '$chakraTheme'],
+  mixins: [createStyledAttrsMixin('CInputAddon')],
   props: addonProps,
   computed: {
-    colorMode () {
-      return this.$chakraColorMode()
-    },
-    theme () {
-      return this.$chakraTheme()
+    componentStyles () {
+      const bg = { dark: 'whiteAlpha.300', light: 'gray.100' }
+      const _placement = {
+        left: {
+          mr: '-1px',
+          roundedRight: 0,
+          borderRightColor: 'transparent'
+        },
+        right: {
+          order: 1,
+          roundedLeft: 0,
+          borderLeftColor: 'transparent'
+        }
+      }
+
+      return {
+        ...useInputStyle({
+          size: this.size,
+          variant: 'outline',
+          colorMode: this.colorMode,
+          theme: this.theme
+        }),
+        flex: '0 0 auto',
+        whiteSpace: 'nowrap',
+        bg: bg[this.colorMode],
+        ..._placement[this.placement]
+      }
     }
   },
   render (h) {
-    const bg = { dark: 'whiteAlpha.300', light: 'gray.100' }
-    const _placement = {
-      left: {
-        mr: '-1px',
-        roundedRight: 0,
-        borderRightColor: 'transparent'
-      },
-      right: {
-        order: 1,
-        roundedLeft: 0,
-        borderLeftColor: 'transparent'
-      }
-    }
-
-    const styleProps = {
-      ...useInputStyle({
-        size: this.size,
-        variant: 'outline',
-        colorMode: this.colorMode,
-        theme: this.theme
-      }),
-      flex: '0 0 auto',
-      whiteSpace: 'nowrap',
-      bg: bg[this.colorMode],
-      ..._placement[this.placement]
-    }
-
-    return h(CBox, {
-      props: {
-        ...forwardProps(this.$props),
-        ...styleProps
-      },
-      attrs: {
-        'data-chakra-component': 'CInputAddon'
-      }
+    return h(this.as || 'div', {
+      class: [this.className],
+      attrs: this.computedAttrs,
+      on: this.computedListeners
     }, this.$slots.default)
   }
 }
@@ -95,17 +83,19 @@ const CInputAddon = {
  */
 const CInputLeftAddon = {
   name: 'CInputLeftAddon',
+  functional: true,
   props: addonProps,
-  render (h) {
+  render (h, { props, slots, data }) {
     return h(CInputAddon, {
       props: {
-        ...forwardProps(this.$props),
+        ...forwardProps(props),
         placement: 'left'
       },
       attrs: {
+        ...data.attrs,
         'data-chakra-component': 'CInputLeftAddon'
       }
-    }, this.$slots.default)
+    }, slots().default)
   }
 }
 
@@ -119,17 +109,19 @@ const CInputLeftAddon = {
  */
 const CInputRightAddon = {
   name: 'CInputRightAddon',
+  functional: true,
   props: addonProps,
-  render (h) {
+  render (h, { props, slots, data }) {
     return h(CInputAddon, {
       props: {
-        ...forwardProps(this.$props),
+        ...forwardProps(props),
         placement: 'right'
       },
       attrs: {
+        ...data.attrs,
         'data-chakra-component': 'CInputRightAddon'
       }
-    }, this.$slots.default)
+    }, slots().default)
   }
 }
 

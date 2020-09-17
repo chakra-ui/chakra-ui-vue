afcec391a57022c26276e4583dfe643c45d3190f
Ken Love <love121@gmail.com>
2020-09-17 01:11:43 -0400

    test: that CReset injects styles

--- packages/chakra-ui-core/src/CReset/tests/CReset.test.js
+++ packages/chakra-ui-core/src/CReset/tests/CReset.test.js
@@ -2,19 +2,25 @@ import CReset from '../'
 import { render, defaultProviders } from '@/tests/test-utils'
 
 const renderComponent = (options = {}) => {
-    return render(
-        CReset, 
-        options, 
-        () => ({ provide: defaultProviders() })
-    )
+  return render(
+    CReset,
+    options,
+    () => ({ provide: defaultProviders() })
+  )
 }
 
 describe('CReset.vue', () => {
-
   it('should render correctly', () => {
     const { asFragment } = renderComponent()
 
     expect(asFragment()).toMatchSnapshot()
   })
 
+  it('should inject global styles', () => {
+    const { container } = renderComponent({
+      container: document
+    })
+
+    expect(container.head).toMatchSnapshot()
+  })
 })

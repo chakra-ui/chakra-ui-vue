65739fb4041ad458f9c9c49edd42ef7b139897b8
IHIutch <jbhutch01@gmail.com>
2021-02-03 21:16:19 -0500

    Set ccollapse height to auto on enter complete, unless finalHeight was specifiied

--- packages/chakra-ui-core/src/CAccordion/CAccordion.stories.js
+++ packages/chakra-ui-core/src/CAccordion/CAccordion.stories.js
@@ -32,6 +32,7 @@ storiesOf('UI | Accordion', module)
           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
           veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
           commodo consequat.
+        </CAccordionPanel>
       </CAccordionItem>
     </CAccordion>
     `

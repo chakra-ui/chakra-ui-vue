a872dc1dedec31711223a9a0cf75523965876589
simpletron <thongitbk@gmail.com>
2021-07-16 17:53:40 +0700

    Remove redundant tx

--- packages/chakra-ui-core/src/utils/index.js
+++ packages/chakra-ui-core/src/utils/index.js
@@ -1,5 +1,5 @@
 export { default as Logger } from './logger'
-export { transformAlias as tx, valueToPercent } from './transform'
+export { valueToPercent } from './transform'
 export { pickProperty as forwardProps } from './object'
 export * from './object'
 export * from './color'
